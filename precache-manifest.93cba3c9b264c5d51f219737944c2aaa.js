c32e51828c1ce1eafe1d3f2d022912ba3a5d732d
Frej <frej@Frejs-MBP-3.lan>
2019-01-29 15:14:40 +0100

    Updates

---
+++ precache-manifest.93cba3c9b264c5d51f219737944c2aaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5533abfbca91ee70d103",
+    "url": "/react-survey-tool/static/js/runtime~main.5533abfb.js"
+  },
+  {
+    "revision": "3c60204329a9c232ec45",
+    "url": "/react-survey-tool/static/js/main.3c602043.chunk.js"
+  },
+  {
+    "revision": "f8a7bb201125b0848f6f",
+    "url": "/react-survey-tool/static/js/1.f8a7bb20.chunk.js"
+  },
+  {
+    "revision": "3c60204329a9c232ec45",
+    "url": "/react-survey-tool/static/css/main.30c181c7.chunk.css"
+  },
+  {
+    "revision": "bfe630dce6a3e24517f44ecef4a20a31",
+    "url": "/react-survey-tool/index.html"
+  }
+];
